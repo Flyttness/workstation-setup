b0e8a3b9bb0be552aa88777a0c61015474923d5e
Seb Krueger <email@sebkrueger.info>
2025-03-22 01:57:41 +1300

    Update developer-utilities.sh
    add warp to the developer tools

--- scripts/common/developer-utilities.sh
+++ scripts/common/developer-utilities.sh
@@ -2,3 +2,4 @@ echo
 brew install --cask postman # api interaction tool
 brew install --cask quicklook-json # OSX tool for viewing JSON
 brew install gh # github cli
+brew install --cask warp # warp AI shell
